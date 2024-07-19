7f996e0c880f35929d9cc2e67bba3ff4534f4ee8
Turkiano <turki.saeed.ksa@gmail.com>
2024-07-19 18:29:57 +0300

    adjusting codes

--- src/Program.cs
+++ src/Program.cs
@@ -7,19 +7,19 @@ namespace Practice
     static void Main(string[] args)
     {
 
-      var consoleLogger = new ConsoleLogger(); //superCLass Object
-      var manager = new PersonManager(consoleLogger); //DI subclass Object 
+      ConsoleLogger consoleLogger = new (); //superCLass Object
+      PersonManager manager = new (consoleLogger); //DI subclass Object 
 
 
       var fileLogger = new FlieLogger(); //superCLass Object
       var manager2 = new  PersonManager(fileLogger); //DI subclass Object 
 
-      PersonManager testing = new (fileLogger);
 
-      manager.AddPerson("Turki");//add the new person
-      manager2.AddPerson("Turki is the best one");
 
-      testing.AddPerson("Satam in the building");
+
+      manager.AddPerson("Turki");//add the new person using the Console
+      manager2.AddPerson("Turki is the best one"); //add the new person using the File generator
+
 
     }
 
